f981d6ee9367ff4a7f7f5acc4a607d2391fcede4
= <chrislie.snts@gmail.com>
2021-03-21 17:28:55 +0700

    tambah email template

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Mail;
+use App\Mail\TransactionSuccess;
+
 use App\Transaction;
 use App\TransactionDetail;
 use App\TravelPackage;
@@ -94,10 +97,17 @@ class CheckoutController extends Controller
 
     public function success(Request $request, $id)
     {
-        $transaction = Transaction::findOrFail($id);
+        $transaction = Transaction::with(['details','travel_package.galleries','user'])->findOrFail($id);
         $transaction->transaction_status = 'PENDING';
         
         $transaction->save();
+
+        // return $transaction;
+
+        // Kirim Email  E-Ticket ke User
+        Mail::to($transaction->user)->send(
+            new TransactionSuccess($transaction)
+        );
 
         return view('pages.success');
     }
